383357c0a9a288dfb52171658f7bd304fdcf2fc4
Ayush <pareekayush500@gmail.com>
2026-07-10 23:26:29 +0530

    dates and time

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,13 @@
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+
+let mytime =Date.now()
+// console.log(Math.floor(mytime/1000));
+let newDate= new Date()
+let mymonth =newDate.getMonth()
+console.log(mymonth+1);
+
+
+
