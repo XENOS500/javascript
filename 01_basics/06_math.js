322b3df982385c65f24d1ab7e1b988e008d28298
Ayush <pareekayush500@gmail.com>
2026-07-10 20:35:13 +0530

    added math.js

---
+++ 01_basics/06_math.js
@@ -0,0 +1,14 @@
+//  console.log(Math.abs(-7));
+//  console.log(Math.round(4.3));
+//  console.log(Math.ceil(4.3));
+//  console.log(Math.floor(4.3));
+//  console.log(Math.min(1,2,3));
+ 
+//  console.log(Math.random());
+ //generates value between 0 inc. and 1 exc.
+//   console.log((Math.random()*10) + 1);
+const min=1
+const max=5
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+//to generate a number b/w 1 inc. and 5 inc.
+ 
