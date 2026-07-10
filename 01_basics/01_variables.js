5d5da75d2bf476403c179f664dd7b0c4b07d1fe2
Ayush <pareekayush500@gmail.com>
2026-07-10 15:50:41 +0530

    committing variables and datatypes .js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,20 @@ const accountId =144553
 let accountEmail ="pareekayush500@gmail.com"
 var accountPassword ="12345"
 accountCity ="jaipur"
+
+// accountId=2 //not allowed
+accountEmail="234@"
+accountCity="rampur"
+
+let accountstate ;
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
+
+
+//in js consants are declared using const keyword
+// let is preferred over var
+/* 
+prefer not to use var 
+because of issue in blockscope and functional scope
+*/
