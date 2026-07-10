fd6ac5b9d8da81fac85acd58e49765ce70b49209
Ayush <pareekayush500@gmail.com>
2026-07-10 17:04:18 +0530

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,16 @@
 "use strict" //treat all JS code as newer version
 
-alert("hello")
+// alert("hello") // we are using nodejs, not browser
+ let age =18
+//code readability should be high
+
+//number 2 to the power 53
+//bigint
+//string =""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned yet
+
+
+//object
+console.log(typeof undefined)
