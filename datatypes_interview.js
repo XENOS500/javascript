d90ce2ef93baa1b77878d9673759ea3ba005c24c
Ayush <pareekayush500@gmail.com>
2026-07-10 18:53:17 +0530

    firse sab kardia

---
+++ datatypes_interview.js
@@ -0,0 +1,9 @@
+//primitive
+
+//these are call by value ,use a copy is passes and changes are made to it
+//7types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+//Reference (Non Primitive)
+
+//Array, Objects, Functions
